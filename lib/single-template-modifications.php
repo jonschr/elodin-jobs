eef4bc416ad50761dd8f203fde47fe3fdd35dd58
jonschr <jonschr@gmail.com>
2020-05-19 18:43:01 -0500

    Adidng the widget areas

--- lib/single-template-modifications.php
+++ lib/single-template-modifications.php
@@ -45,9 +45,54 @@ function elodin_do_jobs_sidebar() {
 // ADD A WIDGET AREA BEFORE THE CONTENT AREA //
 ///////////////////////////////////////////////
 
+//* Register the widget area
+add_action( 'init', 'elodin_jobs_register_widget_area_above' );
+function elodin_jobs_register_widget_area_above() {
+    genesis_register_sidebar( array(
+        'id'		=> 'before-single-job',
+        'name'		=> __( 'Job listings (above)', 'elodin-jobs' ),
+        'description'	=> __( 'An area intended to house any short-form legal language which should be included with every job listing.', 'elodin-jobs' ),
+    ) );
+}
 
+//* Display the widget area
+add_action( 'genesis_before_entry_content', 'elodin_jobs_add_widget_area_before' );
+function elodin_jobs_add_widget_area_before() {
+
+    //* bail if we're not on a job
+    if ( !is_singular( 'jobs' ) )
+        return;
+        
+	genesis_widget_area( 'before-single-job', array(
+        'before' => '<div class="jobs-widget-wrap-before">',
+        'after' => '</div>',
+	) );
+}
 
 //////////////////////////////////////////////
 // ADD A WIDGET AREA AFTER THE CONTENT AREA //
 //////////////////////////////////////////////
 
+//* Register the widget area
+add_action( 'init', 'elodin_jobs_register_widget_area_below' );
+function elodin_jobs_register_widget_area_below() {
+    genesis_register_sidebar( array(
+        'id'		=> 'after-single-job',
+        'name'		=> __( 'Job listings (below)', 'elodin-jobs' ),
+        'description'	=> __( 'An area intended to house the job application form (or a CTA link to one if the application isn\'t part of this site', 'elodin-jobs' ),
+    ) );
+}
+
+//* Display the widget area
+add_action( 'genesis_after_entry_content', 'elodin_jobs_add_widget_area_after' );
+function elodin_jobs_add_widget_area_after() {
+
+    //* bail if we're not on a job
+    if ( !is_singular( 'jobs' ) )
+        return;
+
+	genesis_widget_area( 'after-single-job', array(
+        'before' => '<div class="jobs-widget-wrap-after">',
+        'after' => '</div>',
+	) );
+}
